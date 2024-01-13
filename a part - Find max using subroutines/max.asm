001d944205877cb3a33dd5b2d6cfd34f799b99b0
aimen056 <98824136+aimen056@users.noreply.github.com>
2024-01-13 23:40:38 +0500

    Update max.asm

--- a part - Find max using subroutines/max.asm
+++ a part - Find max using subroutines/max.asm
@@ -1,1 +1,33 @@
+[org 100h]
+jmp start
 
+
+num1: dw 1, 6, 2, 4, 3
+results: dw 0
+
+max:
+push bp
+mov bp, sp
+mov bx, [bp+6]
+mov dx, [bx]
+
+max_loop:
+cmp [bx], dx
+jle next
+mov dx, [bx]
+next:
+add bx, 2
+loop max_loop
+mov [results], dx
+pop bp
+ret 4
+
+start:
+mov ax, num1
+push ax
+mov cx, 5
+push cx
+call max
+
+mov ax, 4ch
+int 21h
